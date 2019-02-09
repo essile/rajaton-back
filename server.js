16018c6097082cdc505ad0be1ebc986fd21b11a8
esle <essi.lehtola@hotmail.com>
2019-02-09 08:17:30 +0200

    cors problem solved

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+
+var cors = require('cors');
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
+
 
 app.get('/api/hello', (req, res) => {
   res.json({ test: 'Hello' });
