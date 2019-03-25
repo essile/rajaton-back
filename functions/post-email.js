93240251102e650194ca088979b655b41146a5bb
esle <essi.lehtola@hotmail.com>
2019-03-25 20:16:36 +0200

    testing options again

--- functions/post-email.js
+++ functions/post-email.js
@@ -8,15 +8,13 @@ app.use(cors({
     'allowedHeaders': ['sessionId', 'Content-Type'],
     'exposedHeaders': ['sessionId'],
     'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
+    'preflightContinue': true,
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }));
 
 exports.handler = function (event, context, callback) {
     console.log(event.httpMethod);
-    if(event.httpMethod === "OPTIONS") {
-        return { statusCode: 200, "Access-Control-Allow-Origin": "*" }
-    }
+
     if (event.httpMethod !== "POST") {
         return { statusCode: 405, body: "Method Not Allowed" };
     }
